ac2b51d94c3300c88d39af6a9350f7c9b7ac77ee
zj <suke.shou@gmail.com>
2018-11-19 22:57:35 +0800

    Add skeleton for NavList

---
+++ src/components/NavList/components/List.js
@@ -0,0 +1,11 @@
+import styled from 'styled-components'
+
+export default styled.ul`
+  margin-top: 20px;
+  padding-left: 0;
+  padding-top: 0;
+  padding-bottom: 0;
+  margin-bottom: 0;
+  list-style: none;
+  position: unset;
+`
---
+++ src/components/NavList/components/ListItemText.js
@@ -0,0 +1,8 @@
+import styled from 'styled-components'
+
+export default styled.div`
+  margin: 0;
+  padding: 0;
+  font-size: 14px;
+  color: #FFFFFF;
+`
